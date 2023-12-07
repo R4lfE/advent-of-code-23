dafb21e1847537bf13e422197822173229b21b50
Ralf Eemers <ralf-eemers@hotmail.com>
2023-12-07 16:50:39 +0100

    cleaner day07

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,25 +1,35 @@
-use std::{cmp::Ordering, error::Error, fs};
+use std::{cmp::Ordering, error::Error, fs, time::Instant};
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    Five = 6,
+    Four = 5,
+    FullHouse = 4,
+    Three = 3,
+    TwoPair = 2,
+    Pair = 1,
+    High = 0
+}
 
 #[derive(Debug, Eq, Ord)]
-enum Hand {
-    Five([u8; 5], u32),
-    Four([u8; 5], u32),
-    FullHouse([u8; 5], u32),
-    Three([u8; 5], u32),
-    TwoPair([u8; 5], u32),
-    Pair([u8; 5], u32),
-    High([u8; 5], u32)
+struct Hand {
+    bid: u32,
+    cards: [u8; 5],
+    hand_type: HandType
 }
 
 impl Hand {
     fn from(s: &str, jokers: u8) -> Self {
         let split: Vec<&str> = s
             .split_whitespace()
+            .map(|line| line.trim())
             .collect();
 
-        let mut hand: [u8; 5] = [0; 5];
+        let bid: u32 = split[1].parse().unwrap();
+
+        let mut cards: [u8; 5] = [0; 5];
         for (i, c) in split[0].chars().enumerate() {
-            hand[i] = match c {
+            cards[i] = match c {
                 'A' => 14 - jokers,
                 'K' => 13 - jokers,
                 'Q' => 12 - jokers,
@@ -30,10 +40,8 @@ impl Hand {
             }
         }
 
-        let bid: u32 = split[1].parse().unwrap();
-
         let mut counter = vec![0; 13];
-        for card in hand.iter() {
+        for card in cards.iter() {
             counter[*card as usize + jokers as usize - 2] += 1;
         }
 
@@ -47,92 +55,64 @@ impl Hand {
             counter[max_index] += counter[0];
         }
 
-        match counter.iter().skip(jokers as usize).max() {
+        let hand_type = match counter.iter().skip(jokers as usize).max() {
             Some(max) => match max {
-                5 => Hand::Five(hand, bid),
-                4 => Hand::Four(hand, bid),
+                5 => HandType::Five,
+                4 => HandType::Four,
 
                 3 => match counter.iter().skip(jokers as usize).filter(|count| **count != 0).min() {
                     Some(min) => match min {
-                        2 => Hand::FullHouse(hand, bid),
-                        1 => Hand::Three(hand, bid),
+                        2 => HandType::FullHouse,
+                        1 => HandType::Three,
                         _ => panic!("Error: Too many cards after three of a kind.")
                     },
                     None => panic!("Error: Not enough cards after three of a kind."),
                 },
                 2 => match counter.iter().skip(jokers as usize).filter(|count| **count == 2).count() {
-                    2 => Hand::TwoPair(hand, bid),
-                    1 => Hand::Pair(hand, bid),
+                    2 => HandType::TwoPair,
+                    1 => HandType::Pair,
                     0 => match jokers {
-                        1 => Hand::Pair(hand, bid),
+                        1 => HandType::Pair,
                         _ => panic!("Error: Wrong number of cards after pair with jokers.")
                     }, 
                     _ => panic!("Error: Wrong number of cards after pair.")
                 },
-                1 => Hand::High(hand, bid),
+                1 => HandType::High,
                 x => panic!("Error: Found {x} cards.")
             },
             None => panic!("Error: Counter is broken."),
+        };
+
+        Hand {
+            bid,
+            cards,
+            hand_type,
         }
     }
 }
 
 impl PartialEq for Hand {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Five(l0, _), Self::Five(r0, _)) => l0 == r0,
-            (Self::Four(l0, _), Self::Four(r0, _)) => l0 == r0,
-            (Self::FullHouse(l0, _), Self::FullHouse(r0, _)) => l0 == r0,
-            (Self::Three(l0, _), Self::Three(r0, _)) => l0 == r0,
-            (Self::TwoPair(l0, _), Self::TwoPair(r0, _)) => l0 == r0,
-            (Self::Pair(l0, _), Self::Pair(r0, _)) => l0 == r0,
-            (Self::High(l0, _), Self::High(r0, _)) => l0 == r0,
-            _ => false,
-        }
+        self.cards == other.cards && self.hand_type == other.hand_type
     }
 }
 
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match (self, other) {
-            (Hand::Five(l, _), Hand::Five(r, _))
-            | (Hand::Four(l, _), Hand::Four(r, _))
-            | (Hand::FullHouse(l, _), Hand::FullHouse(r, _))
-            | (Hand::Three(l, _), Hand::Three(r, _))
-            | (Hand::TwoPair(l, _), Hand::TwoPair(r, _))
-            | (Hand::Pair(l, _), Hand::Pair(r, _))
-            | (Hand::High(l, _), Hand::High(r, _)) => {
-                for i in 0..l.len() {
-                    if l[i] < r[i] {
-                        return Some(Ordering::Less);
-                    } else if l[i] > r[i] {
-                        return Some(Ordering::Greater);
-                    }
-                } 
-                return Some(Ordering::Equal);
-            },
-            (Hand::Five(_, _), Hand::Four(_, _))
-            | (Hand::Five(_, _), Hand::FullHouse(_, _))
-            | (Hand::Five(_, _), Hand::Three(_, _))
-            | (Hand::Five(_, _), Hand::TwoPair(_, _))
-            | (Hand::Five(_, _), Hand::Pair(_, _))
-            | (Hand::Five(_, _), Hand::High(_, _))
-            | (Hand::Four(_, _), Hand::FullHouse(_, _))
-            | (Hand::Four(_, _), Hand::Three(_, _))
-            | (Hand::Four(_, _), Hand::TwoPair(_, _))
-            | (Hand::Four(_, _), Hand::Pair(_, _))
-            | (Hand::Four(_, _), Hand::High(_, _))
-            | (Hand::FullHouse(_, _), Hand::Three(_, _))
-            | (Hand::FullHouse(_, _), Hand::TwoPair(_, _))
-            | (Hand::FullHouse(_, _), Hand::Pair(_, _))
-            | (Hand::FullHouse(_, _), Hand::High(_, _))
-            | (Hand::Three(_, _), Hand::TwoPair(_, _))
-            | (Hand::Three(_, _), Hand::Pair(_, _))
-            | (Hand::Three(_, _), Hand::High(_, _))
-            | (Hand::TwoPair(_, _), Hand::Pair(_, _))
-            | (Hand::TwoPair(_, _), Hand::High(_, _))
-            | (Hand::Pair(_, _), Hand::High(_, _)) => Some(Ordering::Greater),
-            _ => Some(Ordering::Less)
+        // compare cards
+        if self.hand_type == other.hand_type {
+            for i in 0..self.cards.len() {
+                if self.cards[i] < other.cards[i] {
+                    return Some(Ordering::Less);
+                } else if self.cards[i] > other.cards[i] {
+                    return Some(Ordering::Greater);
+                }
+            }
+            return None;
+
+        // compare hand types
+        } else {
+            return self.hand_type.partial_cmp(&other.hand_type);
         }
     }
 }
@@ -149,22 +129,19 @@ fn both(input: &str, jokers: u8) -> u32 {
     hands
         .iter()
         .enumerate()
-        .fold(0, |acc, (i, hand)| acc + match hand {
-            Hand::Five(_, bid)
-            | Hand::Four(_, bid)
-            | Hand::FullHouse(_, bid)
-            | Hand::Three(_, bid)
-            | Hand::TwoPair(_, bid)
-            | Hand::Pair(_, bid)
-            | Hand::High(_, bid) => bid * (i as u32 + 1)
-        })
+        .fold(0, |acc, (i, hand)| acc + hand.bid * (i as u32 + 1))
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input.txt")?;
     
+    let mut now = Instant::now();
     dbg!(both(&input, 0));
+    println!("{}", now.elapsed().as_nanos());
+
+    now = Instant::now();
     dbg!(both(&input, 1));
+    println!("{}", now.elapsed().as_nanos());
 
     Ok(())
 }
@@ -175,11 +152,11 @@ mod tests {
 
     #[test]
     fn card() {
-        assert_eq!(Hand::from("JQQQQ 1", 1), Hand::Five([1,11,11,11,11], 1));
-        assert_eq!(Hand::from("JQ4QQ 1", 1), Hand::Four([1,11,4,11,11], 1));
-        assert_eq!(Hand::from("JQQ44 1", 1), Hand::FullHouse([1,11,11,4,4], 1));
-        assert_eq!(Hand::from("JQQ53 1", 1), Hand::Three([1,11,11,5,3], 1));
-        assert_eq!(Hand::from("J32QK 1", 1), Hand::Pair([1,3,2,11,12], 1));
+        assert_eq!(Hand::from("JQQQQ 1", 1).hand_type, HandType::Five);
+        assert_eq!(Hand::from("JQ4QQ 1", 1).hand_type, HandType::Four);
+        assert_eq!(Hand::from("JQQ44 1", 1).hand_type, HandType::FullHouse);
+        assert_eq!(Hand::from("JQQ53 1", 1).hand_type, HandType::Three);
+        assert_eq!(Hand::from("J32QK 1", 1).hand_type, HandType::Pair);
     }
 
     #[test]
