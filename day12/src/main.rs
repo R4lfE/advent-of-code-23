a1d9a51ca84eac5b69fd2422565a64f89d68f99b
Ralf Eemers <ralf-eemers@hotmail.com>
2023-12-15 18:12:04 +0100

    fixed day12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,6 +1,7 @@
+use regex::Regex;
 use std::{error::Error, fs};
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 enum Spring {
     Operational,
     Damaged,
@@ -13,29 +14,31 @@ impl From<char> for Spring {
             '.' => Self::Operational,
             '#' => Self::Damaged,
             '?' => Self::Unknown,
-            _ => panic!("Error: Invalid spring found")
+            _ => panic!("Error: Invalid character '{value}'.")
         }
     }
 }
 
-fn can_place(springs: &[Spring], groups: &[u64], i_spring: usize, i_group: usize) -> bool {
+fn can_place(springs: &[Spring], groups: &[usize], i_spring: usize, i_group: usize) -> bool {
     // we cannot place the group
     // if the group cannot fit on the springs
-    !(i_spring + 1 < i_group + groups[..i_group + 1].iter().sum::<u64>() as usize
+    !(i_spring + 1 < i_group + groups[..=i_group].iter().sum::<usize>()
 
-    || (i_spring > groups[i_group] as usize 
     // or if the spring before the group is damaged
-    && (springs[i_spring - groups[i_group] as usize] == Spring::Damaged
+    || (i_spring >= groups[i_group]
+        && (springs[i_spring - groups[i_group]] == Spring::Damaged)
 
     // or if some spring in the group range is operational
-    || springs[i_spring - groups[i_group] as usize + 1..i_spring].iter().any(|spring| *spring == Spring::Operational))))
+    || (i_spring + 1 >= groups[i_group]
+        && springs[i_spring + 1 - groups[i_group]..i_spring].iter().any(|spring| *spring == Spring::Operational))))
 
     // otherwise we can now place the group starting at S[j - G[i] + 1]
 }
 
-fn tabulated(springs: Vec<Spring>, groups: Vec<u64>) -> u64 {
+fn tabulated(springs: Vec<Spring>, groups: Vec<usize>) -> usize {
     let mut table = vec![vec![0_usize; springs.len() + 1]; groups.len() + 1];
 
+    // solution is valid when no groups or damaged springs exist
     let first_damaged = springs.iter().position(|spring| *spring == Spring::Damaged).unwrap_or(springs.len());
     for j in 0..=first_damaged {
         table[0][j] = 1;
@@ -44,67 +47,62 @@ fn tabulated(springs: Vec<Spring>, groups: Vec<u64>) -> u64 {
     for i in 1..=groups.len() {
         for j in 1..=springs.len() {
 
-            match springs[j - 1] {
-                // no effect, give number of solutions of previous spring
-                Spring::Operational => {
-                    table[i][j] = table[i][j - 1];
-                },
-                // number of ways we can arrange previous groups up to current group placement
-                // only if we can place the current group
-                Spring::Damaged => {
-                    table[i][j] = if can_place(&springs, &groups, j - 1, i - 1) {
-                        let before_operational = if j > groups[i - 1] as usize {
-                            1
-                        } else {
-                            0
-                        };
-                        table[i - 1][j - groups[i - 1] as usize - before_operational]
-                    } else {
-                        0
-                    };
-                },
-                // sum of trying to place the group and considering this spring operational
-                Spring::Unknown => {
-                    table[i][j] = if can_place(&springs, &groups, j - 1, i - 1) {
-                        let before_operational = if j > groups[i - 1] as usize {
-                            1
-                        } else {
-                            0
-                        };
-                        table[i - 1][j - groups[i - 1] as usize - before_operational]
-                    } else {
-                        0
-                    };
-                    table[i][j] += table[i][j - 1];
-                },
+            // no effect, give number of solutions of previous sub problem
+            if springs[j - 1] == Spring::Operational || springs[j - 1] == Spring::Unknown {
+                table[i][j] += table[i][j - 1];
             }
+            
+            // number of ways we can arrange previous groups up to current group placement
+            // only if we can place the current group on S[j - G[i] + 1]
+            if (springs[j - 1] == Spring::Damaged || springs[j - 1] == Spring::Unknown)
+                && can_place(&springs, &groups, j - 1, i - 1) {
+
+                table[i][j] += table[i - 1][j - groups[i - 1] - (i > 1) as usize];
+            }
+
+            // unkown is the sum of trying to place the group and considering this spring operational
         }
     }
 
-    table[groups.len()][springs.len()] as u64
+    table[groups.len()][springs.len()] as usize
 }
 
-fn part1(input: &str) -> u64 {
+fn part1(input: &str) -> usize {
     input
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| {
+            let re = Regex::new(r"\.+").unwrap();
             let split: Vec<&str> = line.split_whitespace().collect();
-            let springs: Vec<Spring> = split[0].chars().map(Spring::from).collect();
-            let groups: Vec<u64> = split[1].split(',').map(|group| group.parse().unwrap()).collect();
+
+            let springs: Vec<Spring> = re
+                .replace_all(split[0], ".")
+                .trim_matches('.')
+                .chars()
+                .map(Spring::from)
+                .collect();
+
+            let groups: Vec<usize> = split[1].split(',').map(|group| group.parse().unwrap()).collect();
 
             tabulated(springs, groups)
         }).sum()
 }
 
-fn part2(input: &str) -> u64 {
+fn part2(input: &str) -> usize {
     input
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| {
+            let re = Regex::new(r"\.+").unwrap();
             let split: Vec<&str> = line.split_whitespace().collect();
-            let springs: Vec<Spring> = [split[0]; 5].join("?").chars().map(Spring::from).collect();
-            let groups: Vec<u64> = [split[1]; 5].join(",").split(',').map(|group| group.parse().unwrap()).collect();
+            let springs: Vec<Spring> = re
+                .replace_all([split[0]; 5].join("?").as_str(), ".")
+                .trim_matches('.')
+                .chars()
+                .map(Spring::from)
+                .collect();
+
+            let groups: Vec<usize> = [split[1]; 5].join(",").split(',').map(|group| group.parse().unwrap()).collect();
 
             tabulated(springs, groups)
         }).sum()
@@ -125,7 +123,7 @@ mod tests {
 
     #[test]
     fn part_1() {
-        let input = "??? 1,1";
+        let input = "????? 3,1";
         assert_eq!(part1(input), 1);
         let input = "???.### 1,1,3";
         assert_eq!(part1(input), 1);
